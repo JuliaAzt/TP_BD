29ead4c5b63962beb20c725c1bfe0d2a84d6d956
deboralagemb <debora.barbosa@aluno.ufop.edu.br>
2019-04-13 20:45:54 -0300

    Adicionado tela home backend

--- application/views/backend/home.php
+++ application/views/backend/home.php
@@ -16,7 +16,40 @@
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <h2>Bem vindo ao sistema <?php echo $this->session->userdata('userlogado')->nome?>!</h2>
+                                    <h2>Saudações, <?php echo $this->session->userdata('userlogado')->nome?>!</h2>
+                                      <div class="container marketing">
+                                        <div class="row" style="margin-top:100px" >
+                                          <div class="col-lg-4" style="margin-bottom: 20px">
+                                            <center>
+                                                <a href="#" title="Link para página de Turmas">
+                                                <img class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" label="Placeholder: 140x140" src="<?php echo base_url('assets/backend/Group-02.png'); ?>" style="border-radius: 50%; background-color:dimgrey">
+                                                </a>
+                                            </center>
+                                            <h2 style="text-align: center" ><a href="#" style="color: black">Turmas</a></h2>
+                                          </div><!-- /.col-lg-4 -->
+                                          <div class="col-lg-4" style="margin-bottom: 20px">
+                                            <center>
+                                                <a href="#" title="Link para página de exercícios">
+                                                <img class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" label="Placeholder: 140x140" src="<?php echo base_url('assets/backend/Pencil-04.png'); ?>"  style="border-radius: 50%; background-color:dimgrey">
+                                                </a>
+                                            </center>
+                                              <h2 style="text-align: center"><a href="#" style="color: black">Exercícios</a></h2>
+                                          </div><!-- /.col-lg-4 -->
+                                          <div class="col-lg-4" style="margin-bottom: 20px">
+                                            <center>
+                                                <a href="#" title="Link para página das Listas de Exercícios">
+                                                <img class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" label="Placeholder: 140x140" src="<?php echo base_url('assets/backend/List-02.png'); ?>"  style="border-radius: 50%; background-color:dimgrey">
+                                                </a>
+                                            </center>    
+                                              <h2 style="text-align: center"><a href="#" style="color: black">Listas de Exercícios</a></h2>
+                                          </div><!-- /.col-lg-4 -->
+                                        </div><!-- /.row -->
+
+
+    <!-- START THE FEATURETTES -->
+
+
+  </div><!-- /.container -->
                                 </div>
                                 
                             </div>
